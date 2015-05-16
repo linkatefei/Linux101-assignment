e08ce9e674792b15f79629e99f4adb72227e6b3d
wangfei <1649079787@qq.com>
2015-05-15 19:15:59 -0700

    filework

--- shell/AddUsers/addUsers.sh
+++ shell/AddUsers/addUsers.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-#
-# useradd
-# chpasswd
-# chown
-# chmod
-# edquota
-#
+for user in `cat /home/wangfei/learngit/Linux101-assignment/shell/AddUsers/users.txt`
+do
+         useradd $user
+         echo "linux" | passwd -a $user
+         echo "$user create success!"
+done 
+chpasswd < /home/wangfei/learngit/Linux101-assignment/shell/AddUsers/passwd
+
+pwconv
+
+
 
